81be6c8c8294882100cb1513282c2c95637e13d2
BBBYB <58743045+BBBYB@users.noreply.github.com>
2019-12-17 11:06:44 -0800

    Create CSTARInteractiveShelves.ino
    first commit

---
+++ CSTARInteractiveShelves.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
+
+Adafruit_VL6180X vl = Adafruit_VL6180X();
+int ledPin = 22;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  vl.begin();
+}
+
+void loop() {
+  float range = vl.readLux(VL6180X_ALS_GAIN_5);
+  int Light = map(range, 0, 50, 255, 0);
+  analogWrite(ledPin, Light);
+  Serial.print("Lux: ");
+  Serial.println(range);
+
+    delay(50);
+}
